31206d25074b96c45e15d09adb93767b5ad889e4
Gilson Maciel <gmaciel@gmail.com>
2018-04-23 18:39:17 +0200

    Solved two sum problem

---
+++ test/src/TwoNumberSumTest.kt
@@ -0,0 +1,18 @@
+import org.hamcrest.core.Is
+import org.junit.Assert
+import org.junit.Test
+
+class TwoNumberSumTest {
+    private val twoNumber = TwoNumberSum()
+
+    @Test
+    fun shouldFindTheFirstNumbers() {
+        val result = twoNumber.find(intArrayOf(1, 2, 3, 4), 7)
+        Assert.assertThat(result.first, Is.`is`(2))
+        Assert.assertThat(result.second, Is.`is`(3))
+
+        val result2 = twoNumber.find(intArrayOf(2, 7, 11, 15), 9)
+        Assert.assertThat(result2.first, Is.`is`(0))
+        Assert.assertThat(result2.second, Is.`is`(1))
+    }
+}
