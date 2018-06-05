73afea090c04a51a915a0eba826c7961ba25871c
Gilson Maciel <gmaciel@gmail.com>
2018-06-05 16:22:16 +0200

    House keeping

--- src/array/ZigZagOrdering.kt
+++ src/array/ZigZagOrdering.kt
@@ -4,24 +4,23 @@ class ZigZagOrdering {
     fun rearrange(input: IntArray): IntArray {
         input.forEachIndexed { index, i ->
             if ((index + 1) < input.size) {
+                var swap = false
                 var current = input[index]
                 var next = input[index + 1]
 
                 if (index % 2 == 0) {
                     if (current > next) {
-                        input[index + 1] = current
-                        input[index] = next
+                        swap = true
                     }
                 } else {
                     if (current < next) {
-                        input[index + 1] = current
-                        input[index] = next
+                        swap = true
                     }
                 }
-
-//                if (swap) {
-
-//                }
+                if (swap) {
+                    input[index + 1] = current
+                    input[index] = next
+                }
             }
         }
         return input
